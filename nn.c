e32a0813a54d8a4893fe519b561c50be0288f7f0
2911nagu <nagusharma2911@gmail.com>
2016-06-06 11:46:09 +0530

    Create nn.c

---
+++ nn.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+   double no;
+  
+   scanf("%lf",&no);
+   if(no>=0)
+   {
+   	printf("it is a positive number");
+   }
+    else
+    
+    printf("it is negative number");
+    
+	return 0;
+}
